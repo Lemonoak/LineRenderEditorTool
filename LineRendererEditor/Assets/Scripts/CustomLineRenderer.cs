0e16243541833f25154d3dce4d0e32712308f26e
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-04-23 01:54:48 +0200

    Created Circle
    Created Circle
    Created custom editor
    Created custom handles for editor scene view editing

---
+++ LineRendererEditor/Assets/Scripts/CustomLineRenderer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class CustomLineRenderer : MonoBehaviour
+{
+    LineRenderer Render;
+
+    public bool DrawEditorCircle = true;
+
+    public int Sides = 4;
+    public float Radius = 1;
+    public float Width = 1;
+
+    void Start()
+    {
+        //caching the renderer
+        Render = GetComponent<LineRenderer>();
+        //always makes sure looping is true
+        Render.loop = true;
+
+        CreatePoints();
+    }
+
+    //Draws the circle during runtime (also refreshes)
+    public void CreatePoints()
+    {
+        Render.widthMultiplier = Width;
+
+        float DeltaTheta = (Mathf.PI * 2.0f) / Sides;
+        float Theta = 0.0f;
+
+        Render.positionCount = Sides;
+        for (int i = 0; i < Render.positionCount; i++)
+        {
+            Vector3 SidesPosition = new Vector3(Radius * Mathf.Cos(Theta), Radius * Mathf.Sin(Theta), 0.0f);
+            Render.SetPosition(i, SidesPosition);
+            Theta += DeltaTheta;
+        }
+    }
+
+#if UNITY_EDITOR
+    //Draws in editor how the circle should look
+    private void OnDrawGizmos()
+    {
+        if(DrawEditorCircle)
+        {
+            float DeltaTheta = (Mathf.PI * 2.0f) / Sides;
+
+            float Theta = 0.0f;
+
+            Vector3 OldPos = Vector3.zero;
+            for (int i = 0; i < Sides + 1; i++)
+            {
+                Vector3 SidesPosition = new Vector3(Radius * Mathf.Cos(Theta), Radius * Mathf.Sin(Theta), 0.0f);
+                Gizmos.DrawLine(OldPos, transform.position + SidesPosition);
+                OldPos = transform.position + SidesPosition;
+                Theta += DeltaTheta;
+            }
+        }
+    }
+#endif
+}
---
+++ LineRendererEditor/Assets/Scripts/Editor/LineRenderEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CustomLineRenderer))]
+public class LineRenderEditor : Editor
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    public override void OnInspectorGUI()
+    {
+        CustomLineRenderer Selected = (CustomLineRenderer)target;
+
+        EditorGUI.BeginChangeCheck();
+        Selected.Sides = (int)EditorGUILayout.Slider("Sides", Selected.Sides, 0, 100);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(Selected, "Changed Vertex amount");
+            Selected.CreatePoints();
+        }
+
+        Selected.Radius = EditorGUILayout.Slider("Radius", Selected.Radius, 0, 100);
+        Selected.Width = EditorGUILayout.Slider("Width", Selected.Width, 0, 100);
+        if (GUILayout.Button("Refresh"))
+        {
+            Selected.CreatePoints();
+        }
+    }
+
+    //Will be used for task 2
+    public void OnSceneGUI()
+    {
+        CustomLineRenderer Selected = (CustomLineRenderer)target;
+        Handles.color = Color.blue;
+        Selected.Radius = Handles.RadiusHandle(Quaternion.identity, Selected.transform.position, Selected.Radius);
+        Handles.ScaleSlider(Selected.Sides, Selected.transform.position, Selected.transform.right, Quaternion.identity, 1f, 1f);
+    }
+
+}
