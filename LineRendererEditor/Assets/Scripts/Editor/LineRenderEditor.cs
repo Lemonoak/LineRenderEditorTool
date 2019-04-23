f7bfd372baac085a171effa5aae0e13cc9bdb322
Lemonoak <45938598+Lemonoak@users.noreply.github.com>
2019-04-23 13:31:18 +0200

    Fixed Updating and non runtime errors
    Fixed Updating and non runtime errors

--- LineRendererEditor/Assets/Scripts/Editor/LineRenderEditor.cs
+++ LineRendererEditor/Assets/Scripts/Editor/LineRenderEditor.cs
@@ -4,38 +4,50 @@ using UnityEngine;
 [CustomEditor(typeof(CustomLineRenderer))]
 public class LineRenderEditor : Editor
 {
-    private void OnEnable()
-    {
-        
-    }
-
+    //For task 1
     public override void OnInspectorGUI()
     {
         CustomLineRenderer Selected = (CustomLineRenderer)target;
 
         EditorGUI.BeginChangeCheck();
         Selected.Sides = (int)EditorGUILayout.Slider("Sides", Selected.Sides, 0, 100);
-        if (EditorGUI.EndChangeCheck())
-        {
-            Undo.RecordObject(Selected, "Changed Vertex amount");
-            Selected.CreatePoints();
-        }
-
         Selected.Radius = EditorGUILayout.Slider("Radius", Selected.Radius, 0, 100);
         Selected.Width = EditorGUILayout.Slider("Width", Selected.Width, 0, 100);
-        if (GUILayout.Button("Refresh"))
+        Selected.DrawEditorCircle = EditorGUILayout.Toggle("Show Gizmo Circle", Selected.DrawEditorCircle);
+        if (EditorGUI.EndChangeCheck())
         {
-            Selected.CreatePoints();
+            Undo.RecordObject(Selected, "Changed LineRenderer Variables");
+            if (EditorApplication.isPlaying)
+            {
+                Selected.CreatePoints();
+            }
         }
+
+        //This is to update the scene view without having to hover to mouse over it
+        SceneView.RepaintAll();
     }
 
-    //Will be used for task 2
+    //For task 2
     public void OnSceneGUI()
     {
         CustomLineRenderer Selected = (CustomLineRenderer)target;
+
+        Color OldColor = Handles.color;
         Handles.color = Color.blue;
+
+        EditorGUI.BeginChangeCheck();
         Selected.Radius = Handles.RadiusHandle(Quaternion.identity, Selected.transform.position, Selected.Radius);
-        Handles.ScaleSlider(Selected.Sides, Selected.transform.position, Selected.transform.right, Quaternion.identity, 1f, 1f);
+        Selected.Sides = (int)Handles.ScaleSlider(Selected.Sides, Selected.transform.position, Selected.transform.right, Quaternion.identity, HandleUtility.GetHandleSize(new Vector3(1,1,1)), 1f);
+        if(EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(Selected, "Changed LineRenderer Variables through scene");
+            if (EditorApplication.isPlaying)
+            {
+                Selected.CreatePoints();
+            }
+        }
+
+        Handles.color = OldColor;
     }
 
 }
